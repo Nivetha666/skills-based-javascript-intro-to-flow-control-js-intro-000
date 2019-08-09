61a2134675fc238173455683bc42128740df509b
Nivetha <nivedha.orton@gmail.com>
2019-08-09 05:46:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age>13&&age<19){
+  if(age>=13&&age<=19){
     return "You are a teenager!" ;
   }else{
     return "You are not a teenager" ;
@@ -17,11 +17,11 @@ function teenager(age) {
 
 function ageChecker(age) {
   if(age>=13||age<=19){
-    console.log("You are a teenager!");
+    return "You are a teenager!" ;
   }else if(age<=12){
-    console.log("You are a kid");
-  }else{
-    console.log("you are a grownup");
+    return "You are a kid" ;
+  }else if(age>=20){
+    retrun "you are a grownup" ;
   }
 
 }
